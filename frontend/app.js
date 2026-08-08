0c966f216fce6752d839609180923dcd11e2fd1f
Riyaa9099 <parasthakur9099@gmail.com>
2026-08-09 00:42:44 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -43,7 +43,7 @@ const toggleAlertBtn = document.getElementById('toggleAlert');
 // ======================================================
 
 // File is inside frontend/sounds/soft-alert2.wav
-const alertSound = new Audio('/sounds/soft-alert2.wav?v=2');
+const alertSound = new Audio('/sounds/soft-alert2.wav');
 
 alertSound.preload = 'auto';
 
